852ee55076030d2c52085bf01aabce93aea1b7d4
Phi <phi_p_nguyen@hotmail.com>
2019-02-25 14:53:36 +0100

    Start Database

---
+++ database/migrations/2019_02_25_113625_create_payments_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePaymentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('payment_name');
+            $table->string('description');
+            $table->decimal('value');
+            $table->string('currency');
+            $table->date('date');
+            $table->string('note');
+            $table->string('imageUrl');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payments');
+    }
+}
