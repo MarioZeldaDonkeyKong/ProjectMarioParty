0afd9972a892ff9439f7784ac0a3663dbd9a97df
Aaron Slots <amjslots.informatica.avans@gmail.com>
2019-03-23 13:39:22 +0100

    upd

--- app/Http/Controllers/PaymentsController.php
+++ app/Http/Controllers/PaymentsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\AccountNumber;
 use App\Http\Controllers\Controller;
 use App\Http\Controllers\Mollie\Wrappers\MollieApiWrapper;
 use App\Payment;
@@ -29,16 +30,42 @@ class PaymentsController extends Controller {
 
         $payment->payment_name = request('payment_name');
         $payment->description = request('description');
-        $payment->value = '10.00';
-        $payment->currency = 'EUR';
-        $payment->date = '2018-10-20';
-        $payment->note = 'A note';
-        $payment->imageUrl = 'www.google.nl';
-        $payment->account_Number_id = '1';
+        $payment->value = request('value');
+        $payment->currency = request('currency');
+        $payment->date = request('date');
+        $payment->note = request('note');
+        $payment->imageUrl = request('image');
+        $payment->setCreatedAt(time());
 
-        $payment->save();
+        $payment_count = count(Payment::all());
+        $payment_id = dechex($payment_count);
+        $payment->id = $payment_id;
+
+        $accountNumber = request('account_number');
+
+        $this->storeInDataBase($payment, $accountNumber);
+        $this->storeInMollie($payment, $accountNumber);
 
         return redirect('/payments');
+    }
+
+    private function storeInDataBase(Payment $payment, string $accountNumber){
+        $payment->account_Number_id = AccountNumber::all()->where('account_number',$accountNumber)->pluck('id');
+        $payment->save();
+    }
+
+    private function storeInMollie(Payment $payment, string $accountNumber){
+        $this->mollie->payments()->create([
+            "amount" => [
+                "currency" => $payment->currency,
+                "value" => $payment->value
+            ],
+            "description" => $payment->payment_name,
+            "metadata" => [
+                "order_id" =>  $payment->id
+            ],
+            "details" => $payment->notes
+        ]);
     }
 
     public function payRequest(Payment $payment){
